5ad3341fb02de52298208f58082576e5dd9bc566
govind663 <whitdevil06@gmail.com>
2024-10-09 14:05:36 +0530

    WSRL | Version - 0.75

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -5,11 +5,11 @@ WSRL | Manaage  Product
 @endsection
 
 @push('styles')
-{{-- <style>
+<style>
     .flex-wrap {
         display: none !important;
     }
-</style> --}}
+</style>
 @endpush
 
 @section('content')
